88a56c49f1c133f425a0bffe82e79c948a895dcc
Sean <sean@SeansWokMBP3821.localdomain>
2022-04-14 16:13:17 -0600

    fix deps

--- dist/textAngular.umd.js
+++ dist/textAngular.umd.js
@@ -1024,7 +1024,7 @@ angular.module('textAngularSetup', [])
 @license textAngular
 Author : Austin Anderson
 License : 2013 MIT
-Version 1.5.16
+Version 1.5.18
 
 See README.md or https://github.com/fraywing/textAngular/wiki for requirements and use.
 */
